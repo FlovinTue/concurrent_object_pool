c875afe0de25d7a8b6007f0928c1d27cafd4bde4
Flovin Tue Michaelsen <flovintue@outlook.com>
2019-11-03 20:04:14 +0100

    Fixed some aligment issues

--- concurrent_object_pool/concurrent_object_pool.h
+++ concurrent_object_pool/concurrent_object_pool.h
@@ -21,24 +21,26 @@
 #pragma once
 
 #include <assert.h>
-#include <gdul\concurrent_queue.h>
+#include <concurrent_queue.h>
 #include <atomic>
 
 #undef get_object
 
 namespace gdul {
 
-template <class Object>
+template <class Object, class Allocator = std::allocator<std::uint8_t>>
 class concurrent_object_pool
 {
 public:
+
 	concurrent_object_pool(std::size_t blockSize);
+	concurrent_object_pool(std::size_t blockSize, Allocator& allocator);
 	~concurrent_object_pool();
 
 	inline Object* get_object();
 	inline void recycle_object(Object* object);
 
-	inline std::size_t avaliable() const;
+	inline std::size_t avaliable();
 
 	inline void unsafe_destroy();
 
@@ -47,32 +49,50 @@ private:
 
 	struct block_node
 	{
-		Object* myBlock;
+		std::uint8_t* myBlock;
+		Object* myObjects;
+		std::size_t myByteSize;
 		std::atomic<block_node*> myPrevious;
 	};
 
-	concurrent_queue<Object*> myUnusedObjects;
+	using object_allocator = typename std::allocator_traits<Allocator>::template rebind_alloc<uint8_t>;
+	using node_allocator = typename std::allocator_traits<Allocator>::template rebind_alloc<block_node>;
+
+	Allocator myAllocator;
+	node_allocator myNodeAllocator;
+
+	concurrent_queue<Object*, Allocator> myUnusedObjects;
 
 	std::atomic<block_node*> myLastBlock;
 
 	const std::size_t myBlockSize;
 };
 
-template<class Object>
-inline concurrent_object_pool<Object>::concurrent_object_pool(std::size_t blockSize)
+template<class Object, class Allocator>
+inline concurrent_object_pool<Object, Allocator>::concurrent_object_pool(std::size_t blockSize)
 	: myBlockSize(blockSize)
-	, myUnusedObjects(blockSize)
+	, myUnusedObjects(blockSize, myAllocator)
 	, myLastBlock(nullptr)
 {
 	try_alloc_block();
 }
-template<class Object>
-inline concurrent_object_pool<Object>::~concurrent_object_pool()
+template<class Object, class Allocator>
+inline concurrent_object_pool<Object, Allocator>::concurrent_object_pool(std::size_t blockSize, Allocator & allocator)
+	: myAllocator(allocator)
+	, myNodeAllocator(allocator)
+	, myBlockSize(blockSize)
+	, myUnusedObjects(blockSize, allocator)
+	, myLastBlock(nullptr)
+{
+	try_alloc_block();
+}
+template<class Object, class Allocator>
+inline concurrent_object_pool<Object, Allocator>::~concurrent_object_pool()
 {
 	unsafe_destroy();
 }
-template<class Object>
-inline Object * concurrent_object_pool<Object>::get_object()
+template<class Object, class Allocator>
+inline Object * concurrent_object_pool<Object, Allocator>::get_object()
 {
 	Object* out;
 
@@ -81,56 +101,82 @@ inline Object * concurrent_object_pool<Object>::get_object()
 	}
 	return out;
 }
-template<class Object>
-inline void concurrent_object_pool<Object>::recycle_object(Object * object)
+template<class Object, class Allocator>
+inline void concurrent_object_pool<Object, Allocator>::recycle_object(Object * object)
 {
 	myUnusedObjects.push(object);
 }
-template<class Object>
-inline std::size_t concurrent_object_pool<Object>::avaliable() const
+template<class Object, class Allocator>
+inline std::size_t concurrent_object_pool<Object, Allocator>::avaliable()
 {
-	return static_cast<uint32_t>(myUnusedObjects.size());
+	return myUnusedObjects.size();
 }
-template<class Object>
-inline void concurrent_object_pool<Object>::unsafe_destroy()
+template<class Object, class Allocator>
+inline void concurrent_object_pool<Object, Allocator>::unsafe_destroy()
 {
-	block_node* blockNode(myLastBlock.load(std::memory_order_relaxed));
+	block_node* blockNode(myLastBlock.load(std::memory_order_acquire));
 	while (blockNode) {
 		block_node* const previous(blockNode->myPrevious);
 
-		delete[] blockNode->myBlock;
-		delete blockNode;
+		for (std::size_t i = 0; i < myBlockSize; ++i) {
+			blockNode->myObjects[i].~Object();
+		}
+		myAllocator.deallocate(blockNode->myBlock, blockNode->myByteSize);
+
+		blockNode->~block_node();
+		myNodeAllocator.deallocate(blockNode, 1);
 
 		blockNode = previous;
 	}
 	myLastBlock = nullptr;
 
-	myUnusedObjects.unsafe_clear();
+	myUnusedObjects.unsafe_reset();
 }
-template<class Object>
-inline void concurrent_object_pool<Object>::try_alloc_block()
+template<class Object, class Allocator>
+inline void concurrent_object_pool<Object, Allocator>::try_alloc_block()
 {
-	block_node* expected(myLastBlock.load(std::memory_order_relaxed));
+	block_node* expected(myLastBlock.load(std::memory_order_acquire));
 
 	if (myUnusedObjects.size()) {
 		return;
 	}
 
-	Object* const block(new Object[myBlockSize]);
+	const std::size_t allocSize(myBlockSize * sizeof(Object));
+	const std::size_t align(alignof(Object));
+	const std::size_t alignComp(!(8 < align) ? 0 : align);
+	const std::size_t totalAllocSize(allocSize + alignComp);
 
-	block_node* const desired(new block_node);
+	uint8_t* const block(myAllocator.allocate(totalAllocSize));
+
+	const std::size_t alignMod((uint64_t)block % align);
+	uint8_t* const objectBegin(block + (alignMod ? align - alignMod : 0));
+	Object* const objects((Object*)objectBegin);
+
+	for (std::size_t i = 0; i < myBlockSize; ++i) {
+		new (&objects[i]) (Object);
+	}
+
+	block_node* const desired(myNodeAllocator.allocate(1));
+	new (desired)(block_node);
 	desired->myPrevious = expected;
 	desired->myBlock = block;
+	desired->myByteSize = totalAllocSize;
+	desired->myObjects = objects;
 
 	if (!myLastBlock.compare_exchange_strong(expected, desired)) {
-		delete desired;
-		delete[] block;
+		for (std::size_t i = 0; i < myBlockSize; ++i) {
+			objects[i].~Object();
+		}
+		myAllocator.deallocate(block, totalAllocSize);
+
+		desired->~block_node();
+		myNodeAllocator.deallocate(desired, 1);
 
 		return;
 	}
 
 	for (std::size_t i = 0; i < myBlockSize; ++i) {
-		myUnusedObjects.push(&block[i]);
+		myUnusedObjects.push(&objects[i]);
 	}
 }
 }
